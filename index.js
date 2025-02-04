a897bb3f6013ca3a6106970f144829427578ca4d
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-02-04 19:52:40 +0530

    Server Side Rendring 1.0.0

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require("express");
 const urlRoute=require('./routes/url');
 const URL=require('./models/url')
 const {connectToMongoDB}=require('./connection');
+const path=require('path');
+const { url } = require("inspector");
 
 
 const app=express();
@@ -12,11 +14,21 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
     console.log("Database connected");
 })
 
+app.set('view Engine','ejs');
+app.set('views',path.resolve("./views"))
+
 app.use(express.json());
+
+app.get('/test',async(req,res)=>{
+    const allURLs=await URL.find({});
+    return res.render('home.ejs',{
+        urls:allURLs,
+    });
+})
 
 app.use('/url',urlRoute);
 
-app.get('/:shortId',async (req,res)=>{
+app.get('/url/:shortId',async (req,res)=>{
     const shortId=req.params.shortId;
   const entry=  await URL.findOneAndUpdate({
         shortId
